3540e3cb4c0f7cebb7bbfc800932badf2a13ac56
USER NAME <55030402@kmitl.ac.th>
2018-08-24 15:45:00 +0700

    first year semester 2

---
+++ program.cs/program.cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 255x semaster 1,I am an Engedu freshman.");
+            Console.WriteLine("Year 255x semaster 1,I LeC arn to program to C sharp. ");
+        }
+    }
+}
